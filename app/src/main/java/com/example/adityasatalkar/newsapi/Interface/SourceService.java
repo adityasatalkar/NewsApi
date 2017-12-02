d4f2ae1403af670791105004fa6a2629dd2162eb
ars75 <adu.satalkar@gmail.com>
2017-12-02 10:28:15 -0500

    initial commit

---
+++ app/src/main/java/com/example/adityasatalkar/newsapi/Interface/SourceService.java
@@ -0,0 +1,10 @@
+package com.example.adityasatalkar.newsapi.Interface;
+
+/**
+ * Created by adityasatalkar on 12/2/17.
+ */
+
+public interface SourceService {
+
+
+}
---
+++ app/src/main/java/com/example/adityasatalkar/newsapi/Model/WebSite.java
@@ -0,0 +1,40 @@
+package com.example.adityasatalkar.newsapi.Model;
+
+import java.util.List;
+
+import javax.xml.transform.Source;
+
+/**
+ * Created by adityasatalkar on 12/2/17.
+ */
+
+public class WebSite {
+
+    private String status;
+    private List<Source> sources;
+
+    public WebSite() {
+
+    }
+
+    public WebSite(String status, List<Source> sources) {
+        this.status = status;
+        this.sources = sources;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
+}
